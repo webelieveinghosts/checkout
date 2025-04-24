2b22b9dec562740baaeffab5c6b4b47e2176b493
marvinho <twothreeseven237@outlook.com>
2025-04-24 13:50:11 -0300

    Update clipboard.tsx

--- src/components/payment/clipboard.tsx
+++ src/components/payment/clipboard.tsx
@@ -1,3 +1,3 @@
 "use client"
 
-export const Clipboard = ({ value }: { value: string }) => (<button className="bg-primary w-20 h-10 text-white text-xs font-semibold uppercase rounded-r" onClick={() => navigator.clipboard.writeText(value)}>copiar</button>)
+export const Clipboard = ({ value }: { value: string }) => (<button className="bg-primary w-20 h-10 text-white text-xs font-semibold uppercase rounded-r cursor-pointer transition-all duration-500 hover:bg-primary-hover" onClick={() => navigator.clipboard.writeText(value)}>copiar</button>)
