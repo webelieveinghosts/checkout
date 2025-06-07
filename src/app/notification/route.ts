d455da08df4532da3ed81ff2e4ab42b7afedae63
marvinho <twothreeseven237@outlook.com>
2025-06-07 17:18:21 -0300

    Update route.ts

--- src/app/notification/route.ts
+++ src/app/notification/route.ts
@@ -7,7 +7,7 @@ export async function POST(req: NextRequest) {
     if (searchParams.get("topic") !== "payment") return NextResponse.json({ message: "No valid topic." }, { status: 500 })
 
     const info = await getPaymentById(searchParams.get("id")!)
-    if (!info || info.status != "approved") return NextResponse.json({ message: "No valid payment id." }, { status: 500 })
+    if (!info || info.status != "approved") return NextResponse.json({ message: "No valid payment id." }, { status: 201 })
 
     await successPayment(searchParams.get("id")!)
     return Response.json({ message: "approved" }, { status: 200 })
