dbe4ad89d4a7a8e342c85abb5040b8c390ffd9bf
ykg-wbg <exgutar@gmail.com>
2025-10-30 03:56:28 -0300

    Update payment.tsx

--- src/components/payment/payment.tsx
+++ src/components/payment/payment.tsx
@@ -2,26 +2,48 @@
 
 import { ICardPaymentBrickPayer, ICardPaymentFormData } from "@mercadopago/sdk-react/esm/bricks/cardPayment/type"
 import { Database } from "@/supabase/database"
-import { CardPayment } from "@mercadopago/sdk-react"
-
-import { initMercadoPago } from "@mercadopago/sdk-react"
+import { CardPayment, initMercadoPago } from "@mercadopago/sdk-react"
 import { createPayment } from "@/mercadopago/payment"
+
 initMercadoPago("APP_USR-363935cd-0e0c-4b75-a0c9-a5cc6a9d2646", {
-    locale: "pt-BR"
+  locale: "pt-BR"
 })
 
 export const Payment = ({ payment }: { payment: Database["public"]["Tables"]["transactions"]["Row"] }) => {
-    const handleCardSubmit = async (cardFormData: ICardPaymentFormData<ICardPaymentBrickPayer>) => {
-        const paymentData = await createPayment(payment, cardFormData)
-    }
 
-    const { email, cpf } = payment.summary_information as any
-    return (<CardPayment initialization={{ amount: payment.total, payer: { email, identification: {
-        type: "CPF",
-        number: cpf
-    } } }} customization={{
-        paymentMethods: {
-            maxInstallments: 3
+  const handleCardSubmit = async (cardFormData: ICardPaymentFormData<ICardPaymentBrickPayer>) => {
+    await createPayment(payment, cardFormData)
+  }
+
+  const { email, cpf, name, phone } = payment.summary_information as any
+
+  const [ddd, numero] = phone?.split(" ") ?? ["", ""]
+
+  return (
+    <CardPayment
+      initialization={{
+        amount: payment.total,
+        payer: {
+          email,
+          first_name: name?.split(" ")[0] || "",
+          last_name: name?.split(" ").slice(1).join(" ") || "",
+          identification: {
+            type: "CPF",
+            number: cpf
+          },
+          phone: {
+            area_code: ddd.replace(/\D/g, ""),
+            number: numero.replace(/\D/g, "")
+          }
         }
-    }} onSubmit={handleCardSubmit} onError={(error) => console.error("Error: rendering CardPayment:", error)} />)
+      }}
+      customization={{
+        paymentMethods: {
+          maxInstallments: 3
+        }
+      }}
+      onSubmit={handleCardSubmit}
+      onError={(error) => console.error("Error: rendering CardPayment:", error)}
+    />
+  )
 }
