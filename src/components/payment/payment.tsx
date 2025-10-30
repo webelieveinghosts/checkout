56fbe64347c046ce83d6e407b3a9749da3d4eff7
haxbash <bluemaquinajp@gmail.com>
2025-10-30 04:35:51 -0300

    fix: correct Mercado Pago payment handling

--- src/components/payment/payment.tsx
+++ src/components/payment/payment.tsx
@@ -8,13 +8,11 @@ import { createPayment } from "@/mercadopago/payment"
 initMercadoPago("APP_USR-363935cd-0e0c-4b75-a0c9-a5cc6a9d2646", { locale: "pt-BR" })
 
 export const Payment = ({ payment }: { payment: Database["public"]["Tables"]["transactions"]["Row"] }) => {
-
   const handleCardSubmit = async (cardFormData: ICardPaymentFormData<ICardPaymentBrickPayer>) => {
     await createPayment(payment, cardFormData)
   }
 
-  const { email, cpf, phone } = payment.summary_information as any
-  const [ddd, numero] = phone?.split(" ") ?? ["", ""]
+  const { email, cpf } = payment.summary_information as any
 
   return (
     <CardPayment
@@ -22,8 +20,7 @@ export const Payment = ({ payment }: { payment: Database["public"]["Tables"]["tr
         amount: payment.total,
         payer: {
           email,
-          identification: { type: "CPF", number: cpf },
-          phone: { area_code: ddd.replace(/\D/g, ""), number: numero.replace(/\D/g, "") }
+          identification: { type: "CPF", number: cpf }
         }
       }}
       customization={{ paymentMethods: { maxInstallments: 3 } }}
